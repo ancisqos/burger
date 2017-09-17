a5a1c2270054a272732b1a31fbb78a379e1e2b77
Francis <ancis.arcede@gmail.com>
2017-09-17 13:13:33 -0400

    final revisions

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,5 @@ VALUES ("Fuddruckers Kobe Beef Burger", FALSE);
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Mozzarella Marinara Burger", FALSE);
 
--- INSERT INTO burgers (burger_name, devoured)
--- VALUES ("Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", FALSE);
